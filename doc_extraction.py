212c7559c76b1bb1a2d16b7f9f96447102819dbd
psa777 <pranav.sadanandamlekar@cognizant.com>
2025-06-02 19:03:41 +0530

    successfully reading reading text, image, table from pdf

--- doc_extraction.py
+++ doc_extraction.py
@@ -12,12 +12,8 @@ POPPLER_PATH = r"C:\Users\UTHRAVFST\AppData\Roaming\Microsoft\Windows\Network Sh
 input_folder = r"C:\kathir\all_file_1"
 
 azure_openai_creds_json_path = r"C:\Pranav\azure_openai_creds.json"
-
 with open(azure_openai_creds_json_path, "r") as file:
     openai_creds = json.load(file)
-
-print(openai_creds)
-
 
 az_llm = AzureChatOpenAI(
     deployment_name=openai_creds["deployment_name"],
@@ -35,15 +31,13 @@ def extract_from_pdf(pdf_path, file_name, markdown_file):
     images_output_path = os.path.join(
         input_folder, "extracted_output_1", file_name, "images"
     )
-    os.makedirs(images_output_path, exist_ok=True)
-
     tables_as_images_output_path = os.path.join(
         input_folder, "extracted_output_1", file_name, "tables_as_images"
     )
+    os.makedirs(images_output_path, exist_ok=True)
     os.makedirs(tables_as_images_output_path, exist_ok=True)
 
-    data = extract_text_tables_and_images(pdf_path)
-
+    data = extract_text_tables_images_from_pdf(pdf_path)
     for page, content in data.items():
 
         text, tables, images, page_data = (
@@ -52,7 +46,6 @@ def extract_from_pdf(pdf_path, file_name, markdown_file):
             content["images"],
             content["page"],
         )
-
         table_texts = [
             cell for table in tables for row in table for cell in row if cell
         ]
@@ -60,38 +53,59 @@ def extract_from_pdf(pdf_path, file_name, markdown_file):
         for txt in table_texts:
             text = text.replace(txt, "")
 
-        table_presence = True if len(table_texts) > 0 else False
-
         data[page]["page_plain_text"] = text
-        data[page]["table_presence"] = table_presence
-
         page_full_text = text
 
-        if len(images) > 0:
+        if images:
+            for i, img in enumerate(images):
 
-            # for i, img in enumerate(images):
+                image_path = os.path.join(images_output_path, f"page_{page}_{i}.png")
+                x0, top, x1, bottom = img["x0"], img["top"], img["x1"], img["bottom"]
+                image = page_data.to_image().original
 
-            #     x0, top, x1, bottom = img["x0"], img["top"], img["x1"], img["bottom"]
-            #     image = page_data.to_image().original
+                cropped_img = image.crop((x0, top, x1, bottom))
+                base64_image = get_image_as_base64(cropped_img, image_path)
 
-            #     cropped_img = image.crop((x0, top, x1, bottom))
-            #     print("cropped_img : ", cropped_img)
-            pass
-            # add logic here to save image, read again and convert to base_64
+                messages_base64 = [
+                    HumanMessage(
+                        content=[
+                            {
+                                "type": "text",
+                                "text": "Check whether the image is a logo or if there's any logo present in the image. If there is no logo present, then consider it as a valid image. If the image is valid then summarize the content in the image. Remember to summarize the entire content of the image as it is and no information should be left to be mentioned in the image ONLY IF it is a valid image. Return only None and nothing else if the image is not valid.",
+                            },
+                            {"type": "image_url", "image_url": {"url": base64_image}},
+                        ]
+                    )
+                ]
 
-        if table_presence:
+                response_base64 = az_llm.invoke(messages_base64)
+                image_summary = response_base64.content
+
+                if eval(image_summary) is not None:
+
+                    print("Image present on page:", page)
+                    print(
+                        f"Image summary for page {page}: and image {i} :  {image_summary}"
+                    )
+                    page_full_text += "\n" + image_summary + "\n"
+
+                print("page_full_text : ", page_full_text)
+
+        if table_texts:
+
             print("Table present on page:", page)
-
-            base64_image = capture_page_as_base64(
-                pdf_path, page, tables_as_images_output_path
+            images = convert_from_path(
+                pdf_path, first_page=page, last_page=page, dpi=50
             )
+            image_path = os.path.join(tables_as_images_output_path, f"page_{page}.png")
 
+            base64_image = get_image_as_base64(images[0], image_path)
             messages_base64 = [
                 HumanMessage(
                     content=[
                         {
                             "type": "text",
-                            "text": "Summarize all the tables in the image. Remember to summarize only tables as it is and no information should be left mentioned in the tables.",
+                            "text": "Summarize all the tables in the image. Remember to summarize only tables as it is and no information should be left to be mentioned in the tables. DO NOT change the named entities names or short forms or add your own interpretation. Return only summary and no extra text.",
                         },
                         {"type": "image_url", "image_url": {"url": base64_image}},
                     ]
@@ -100,16 +114,17 @@ def extract_from_pdf(pdf_path, file_name, markdown_file):
 
             response_base64 = az_llm.invoke(messages_base64)
             table_summary = response_base64.content
-            print(f"Table summary for page {page}: {table_summary}")
+            # print(f"Table summary for page {page}: {table_summary}")
 
             page_full_text += "\n" + table_summary + "\n"
             print("page_full_text : ", page_full_text)
 
-        else:
+        data[page]["page_full_text"] = page_full_text
 
-            print("Table not present on page:", page)
+        if len(data[page]["text"]) > len(data[page]["page_full_text"]):
 
-        data[page]["page_plain_text"] = page_full_text
+            data[page]["page_full_text"] = data[page]["text"]
+            print("page : ", page)
 
     print("debugging")
 
@@ -162,13 +177,12 @@ def extract_from_docx(docx_path, file_name):
     os.makedirs(output_path, exist_ok=True)
 
     text = docx2txt.process(docx_path, output_path)
-
     full_text = ""
+
     if len(text.strip()) > 20:
         full_text += f"\n--- Page 1 ---\n{text}\n"
 
     else:
-
         # Text is very short => possibly scanned docx (mostly images)
         # Convert docx to PDF for further processing if needed
         pdf_output_path = os.path.join(output_path, f"{file_name}.pdf")
@@ -183,13 +197,11 @@ def extract_from_docx(docx_path, file_name):
     if full_text:
 
         text_filename = f"{file_name}.txt"
-
         with open(os.path.join(output_path, text_filename), "w", encoding="utf-8") as f:
             f.write(full_text)
 
     # Rename images extracted by docx2txt
     img_counter = 1
-
     for img_file in os.listdir(output_path):
 
         if img_file.lower().startswith("image") and img_file.lower().endswith(
@@ -205,40 +217,28 @@ def extract_from_docx(docx_path, file_name):
             img_counter += 1
 
 
-def separate_text_and_tables_from_markdown(markdown_content):
+def get_image_as_base64(image, image_path):
 
-    lines = markdown_content.split("\n")
-    text = []
-    tables = []
-    current_table = []
+    image = image.resize((600, 600)).convert("L")
+    image.save(image_path, "PNG", quality=50)
 
-    for line in lines:
+    with open(image_path, "rb") as image_file:
 
-        if line.strip().startswith("|"):
-            current_table.append(line)
+        base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        mime_type, _ = guess_type(image_path)
+        image_to_return = f"data:{mime_type};base64,{base64_string}"
 
-        else:
-
-            if current_table:  # End of a table block
-                tables.append("\n".join(current_table))
-                current_table = []
-            text.append(line)
-
-    # Add any remaining table at the end
-    if current_table:
-        tables.append("\n".join(current_table))
-
-    return "\n".join(text), tables
+    return image_to_return
 
 
-def extract_text_tables_and_images(pdf_path):
+def extract_text_tables_images_from_pdf(pdf_path):
 
     page_data = {}
 
     with pdfplumber.open(pdf_path) as pdf:
         for page_num, page in enumerate(pdf.pages):
 
-            if page_num == 8:  # Limit to first 5 pages
+            if page_num == 3:
                 break
 
             text = page.extract_text()
@@ -253,24 +253,6 @@ def extract_text_tables_and_images(pdf_path):
             }
 
     return page_data
-
-
-def capture_page_as_base64(pdf_path, page_num, tables_as_images_output_path):
-
-    images = convert_from_path(
-        pdf_path, first_page=page_num, last_page=page_num, dpi=100
-    )
-    image_path = os.path.join(tables_as_images_output_path, f"page_{page_num}.png")
-    image = images[0].resize((400, 400)).convert("L")
-    image.save(image_path, "WEBP", quality=50)
-
-    with open(image_path, "rb") as image_file:
-
-        base64_string = base64.b64encode(image_file.read()).decode("utf-8")
-        mime_type, _ = guess_type(image_path)
-        image_to_return = f"data:{mime_type};base64,{base64_string}"
-
-    return image_to_return
 
 
 for file in os.listdir(input_folder):
